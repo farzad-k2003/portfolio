d21901398a462bc874a6cbda92b2ad40670b3d67
farzad-k2003 <farzad.1381.kavoosi@gmail.com>
2022-05-31 14:43:52 +0430

    Updated in 5/31/2022 at 1443

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -4,15 +4,16 @@ const Banner = () => {
   return (
     <div className="banner-container">
       <div className="banner">
-        {/* <img
-          src="./edward-howell-FTRkZnSrr1M-unsplash.jpg"
-          alt="banner"
-          className="banner-img"
-        /> */}
-        {/* <img src={`/edward-howell-FTRkZnSrr1M-unsplash.jpg`} /> */}
-        <img src={`/edward-howell-FTRkZnSrr1M-unsplash.jpg`} width="10" />
+        <img src={`/edward-howell-FTRkZnSrr1M-unsplash.jpg`} />
         <div className="border1"></div>
         <div className="border2"></div>
+        <h3>HI THERE! I AM</h3>
+        <h1>Farzad Kavoosi</h1>
+      </div>
+
+      <div className="status">
+        <span></span>
+        <p>I'm available for a freelance job. Hire me</p>
       </div>
     </div>
   );
